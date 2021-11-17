ee3eb9a64b7f6cc618becbc07747938f9b305d9b
Hiroki Ohnuki <hohnuki@c1r29s1.42tokyo.jp>
2021-11-17 19:22:38 +0900

    11/17 %p add

--- ft_put_address.c
+++ ft_put_address.c
@@ -6,11 +6,25 @@
 /*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 18:34:16 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/11/17 18:48:06 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/11/17 19:21:48 by hohnuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//void	ft_put_address()
-//{
+#include "ft_printf.h"
 
+void	ft_put_address(unsigned int c)
+{
+	unsigned long long	address;
+
+	address = (unsigned long long)&c;
+	ft_putstr("0x7ffe");
+	ft_putbit_lowercase(address);
+}
+
+//int main(void)
+//{
+//	int a = 10;
+
+//	printf("%p\n", &a);
+//	ft_put_address(a);
 //}
