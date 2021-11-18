94f5e9e6edfb9f43cca590ece9417adb1ea181ed
大貫　弘貴 <ohnukihiroki@oonukihiroshitakanoMacBook-Pro.local>
2021-11-19 01:16:35 +0900

    11/18 add accuracy&min_field

--- ft_printf.c
+++ ft_printf.c
@@ -3,15 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ohnukihiroki <ohnukihiroki@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 22:38:27 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/11/17 19:21:12 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/11/18 18:21:51 by ohnukihirok      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-//malloc, free, write, va_start, va_arg, va_copy, va_end
+
+size_t	adjust_min_field(char *ptr, size_t index)
+{
+	size_t	i;
+	size_t	shift_num;
+
+	i = index;
+	shift_num = 0;
+	while (ft_isdigit(ptr[i]) == 1)
+	{
+		shift_num *= 10;
+		shift_num += ptr[i]  - '0';
+		i++;
+	}
+	while (shift_num - 1 > 0)
+	{
+		ft_putchar(' ');
+		shift_num--;
+	}
+	//このi-1怪しい
+	return (i - 1);
+}
+
+size_t	adjust_accuracy(char *ptr, size_t index)
+{
+	size_t	i;
+	size_t	shift_num;
+
+	i = index + 1;
+	shift_num = 0;
+	while (ft_isdigit(ptr[i]) == 1)
+	{
+		shift_num *= 10;
+		shift_num += ptr[i]  - '0';
+		i++;
+	}
+	while (shift_num - 1 > 0)
+	{
+		ft_putchar('0');
+		shift_num--;
+	}
+	//このi-1怪しい
+	return (i - 1);
+}
+
 
 int	ft_printf(const char *format, ...)
 {
@@ -22,9 +66,8 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
-		if (format[i] != '%')
-			break ;
-		i++;
+		if (format[i] == '%')
+			i++;
 		if (format[i] == 'd' || format[i] == 'i')
 			ft_putnbr(va_arg(args, int));
 		else if (format[i] == 'c')
@@ -39,8 +82,21 @@ int	ft_printf(const char *format, ...)
 			ft_putbit_uppercase(va_arg(args, unsigned int));
 		else if (format[i] == 'p')
 			ft_put_address(va_arg(args, unsigned int));
+		else if (ft_isdigit(format[i]) == 1)
+			i = adjust_min_field((char *)format, i);
+		else if (format[i] == '.')
+			i = adjust_accuracy((char *)format, i);
+		else
+			ft_putchar(format[i]);
 		i++;
 	}
 	va_end(args);
 	return (0);
+}
+
+int	main(void)
+{
+	int	num = 5;
+	printf("[%30d]\n", num);
+	ft_printf("[%30d]", num);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ohnukihiroki <ohnukihiroki@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 15:08:50 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/11/17 19:20:50 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/11/18 16:48:21 by ohnukihirok      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ void	ft_putstr(char *str);
 void	ft_putnbr_ui(unsigned int nb);
 void	ft_putbit_lowercase(unsigned int c);
 void	ft_putbit_uppercase(unsigned int c);
+void	ft_put_address(unsigned int c);
+int		ft_isdigit(int c);
 
 #endif
