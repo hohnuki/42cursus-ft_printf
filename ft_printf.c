83be4afd70b7ed51ca2faf1adc646ff0cf229c6e
Hiroki Ohnuki <hohnuki@c1r18s9.42tokyo.jp>
2021-11-25 17:38:45 +0900

    11/25 add parse_conversion

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 22:38:27 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/11/25 13:33:41 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/11/25 17:31:54 by hohnuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,31 +38,46 @@ static size_t	divergence_int(int num, char c)
 	return (ret);
 }
 
+static size_t	parse_conversion(const char *format, int i, va_list *args)
+{
+	size_t	ret;
+
+	ret = 0;
+	if (format[i] == 'd' || format[i] == 'i' || format[i] == 'c')
+		ret += divergence_int(va_arg(*args, int), format[i]);
+	else if (format[i] == 'x' || format[i] == 'X' || format[i] == 'u')
+		ret += divergence_ui(va_arg(*args, unsigned int), format[i]);
+	else if (format[i] == 's')
+		ret += ft_putstr(va_arg(*args, char *));
+	else if (format[i] == 'p')
+		ret += ft_put_address(va_arg(*args, unsigned long long));
+	else
+		ret += ft_putchar_c(format[i]);
+	return (ret);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	size_t	i;
-	size_t	re;
+	size_t	ret;
 
 	i = 0;
-	re = 0;
+	ret = 0;
 	va_start(args, format);
+	if (!format)
+		return (-1);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
+		{
 			i++;
-		if (format[i] == 'd' || format[i] == 'i' || format[i] == 'c')
-			re += divergence_int(va_arg(args, int), format[i]);
-		else if (format[i] == 'x' || format[i] == 'X' || format[i] == 'u')
-			re += divergence_ui(va_arg(args, unsigned int), format[i]);
-		else if (format[i] == 's')
-			re += ft_putstr(va_arg(args, char *));
-		else if (format[i] == 'p')
-			re += ft_put_address(va_arg(args, unsigned long long));
+			ret += parse_conversion(format, i, &args);
+		}
 		else
-			re += ft_putchar_c(format[i]);
+			ret += ft_putchar_c(format[i]);
 		i++;
 	}
 	va_end(args);
-	return ((int)re);
+	return ((int)ret);
 }
